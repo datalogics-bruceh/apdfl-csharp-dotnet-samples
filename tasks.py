cb0374a3ce8bf080b490e30802da8bdd9d635f92
Arnulfo Arroyo <aarroyo@datalogics.com>
2025-01-06 20:20:15 -0600

    Fix config name and add skiasharp location

--- tasks.py
+++ tasks.py
@@ -130,7 +130,7 @@ def clean_nuget_cache(ctx):
 
 @task()
 def build_samples(ctx, pkg_name='Adobe.PDF.Library.NET', config='Debug'):
-    """Builds the .NET samples"""
+    """Builds the .NET6 samples"""
     ctx.run('invoke clean-samples')
     for sample in samples_list:
         full_path = os.path.join(os.getcwd(), sample)
